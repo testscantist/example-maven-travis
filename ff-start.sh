bddb7e449631e3f198d8839a46112e663d93508d
Zeng Feifan <turly221@users.noreply.github.com>
2018-04-26 11:44:42 +0800

    Update ff-start.sh

--- ff-start.sh
+++ ff-start.sh
@@ -1,6 +1,7 @@
 # initialize parameters
 # identify pull request and push
 # prepare upload path, request url
+SCANTIST_IMPORT_URL="http://e8482e5c.ngrok.io/import/"
 
 # run Java cmd and store logs
 run_script() {
@@ -15,7 +16,18 @@ run_script() {
   exit $RESULT
 }
 
-run_detect > ./scan/depedency-tree-output.txt
+run_script > depedency-tree-output.txt
 # run script to extrac depedency tree info
 
 # upload to s3 endpoint
+
+#Log that the script download is complete and proceeding
+echo "Uploading report at $1"
+
+#Log the curl version used
+curl --version
+
+curl -g -v -f -X POST -d @depedency-tree-output.txt -H 'Content-Type:application/x-www-form-urlencoded' -H  "$SCANTIST_IMPORT_URL"
+
+#Exit with the curl command's output status
+exit $?
