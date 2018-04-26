886b2c061eeb02d10f07a3888d771d0173917c40
Zeng Feifan <turly221@users.noreply.github.com>
2018-04-26 10:53:15 +0800

    Update ff-start.sh

--- ff-start.sh
+++ ff-start.sh
@@ -3,17 +3,19 @@
 # prepare upload path, request url
 
 # run Java cmd and store logs
-run_detect() {
-  
-  echo "#!/bin/sh" >> $DETECT_JAR_PATH/hub-detect-java.sh
-  echo "" >> $DETECT_JAR_PATH/hub-detect-java.sh
-  echo $JAVACMD $SCRIPT_ARGS >> $DETECT_JAR_PATH/hub-detect-java.sh
-  source $DETECT_JAR_PATH/hub-detect-java.sh
-  RESULT=$?
-  echo "Result code of ${RESULT}, exiting"
-  rm -f $DETECT_JAR_PATH/hub-detect-java.sh
+run_script() {
+  echo $TRAVIS_BRANCH
+  echo $TRAVIS_COMMIT
+  echo $TRAVIS_EVENT_TYPE
+  echo $TRAVIS_PULL_REQUEST
+  echo $TRAVIS_PULL_REQUEST_BRANCH
+  echo $TRAVIS_PULL_REQUEST_SHA
+  echo $TRAVIS_JDK_VERSION
+  mvn depedency:tree
   exit $RESULT
 }
+
+run_detect > ./scan/depedency-tree-output.txt
 # run script to extrac depedency tree info
 
 # upload to s3 endpoint
