daf163b00695161c53c688c34d8474c4f41a969c
zengfeifan <turly221@gmail.com>
2018-04-26 13:42:31 +0800

    check maven output

--- ff-start.sh
+++ ff-start.sh
@@ -8,17 +8,18 @@ SCANTIST_IMPORT_URL="http://e8482e5c.ngrok.io/import/"
 
 # run Java cmd and store logs
 run_script() {
-  echo $TRAVIS_BRANCH
-  echo $TRAVIS_COMMIT
-  echo $TRAVIS_EVENT_TYPE
-  echo $TRAVIS_PULL_REQUEST
-  echo $TRAVIS_PULL_REQUEST_BRANCH
-  echo $TRAVIS_PULL_REQUEST_SHA
-  echo $TRAVIS_JDK_VERSION
+  # echo $TRAVIS_BRANCH
+  # echo $TRAVIS_COMMIT
+  # echo $TRAVIS_EVENT_TYPE
+  # echo $TRAVIS_PULL_REQUEST
+  # echo $TRAVIS_PULL_REQUEST_BRANCH
+  # echo $TRAVIS_PULL_REQUEST_SHA
+  # echo $TRAVIS_JDK_VERSION
   mvn -B dependency:tree
 }
 
 run_script > depedency-tree-output.txt
+cat depedency-tree-output.txt
 # run script to extrac depedency tree info
 
 # upload to s3 endpoint
