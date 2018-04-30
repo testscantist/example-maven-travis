04ecd70aec74f6cae8090dd36249589ffeff18b6
zengfeifan <turly221@gmail.com>
2018-04-30 19:47:52 +0800

    test new script

--- ff-start.sh
+++ ff-start.sh
@@ -6,21 +6,21 @@ echo "inside ff-start.sh script"
 
 SCANTIST_IMPORT_URL="http://e8482e5c.ngrok.io/import/"
 
-# run Java cmd and store logs
-run_script() {
-  # echo $TRAVIS_BRANCH
-  # echo $TRAVIS_COMMIT
-  # echo $TRAVIS_EVENT_TYPE
-  # echo $TRAVIS_PULL_REQUEST
-  # echo $TRAVIS_PULL_REQUEST_BRANCH
-  # echo $TRAVIS_PULL_REQUEST_SHA
-  # echo $TRAVIS_JDK_VERSION
-  mvn -B dependency:tree
-}
-echo "------------------------------------"
-echo "run_script"
 
-run_script > raw-output.txt
+# # run Java cmd and store logs
+# run_script() {
+#   if [ -f pom.xml ]; then
+#     mvn -B dependency:tree
+#   fi
+#   if [ -f build.gradle ]; then
+#     gradle dependencies --configuration compile
+#   fi
+
+# }
+# echo "------------------------------------"
+# echo "run_script"
+
+# run_script > raw-output.txt
 
 # run script to extrac depedency tree info
 chmod a+x TreeBuilder
@@ -28,6 +28,8 @@ cwd=$(pwd)
 ls
 echo $cwd
 ./TreeBuilder $cwd
+
+cat raw-output.txt
 
 #Log that the script download is complete and proceeding
 echo "Uploading report at $SCANTIST_IMPORT_URL"
